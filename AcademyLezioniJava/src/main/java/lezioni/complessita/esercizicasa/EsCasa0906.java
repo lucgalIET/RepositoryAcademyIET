991b9f3520b07dfb4f222316fff8a254a893dee3
Luca Galiero <luca.galiero@iet.it>
2023-06-09 16:57:34 +0200

    complessita': Esercizi per casa 09-06-23

---
+++ AcademyLezioniJava/src/main/java/lezioni/complessita/esercizicasa/EsCasa0906.java
@@ -0,0 +1,14 @@
+package main.java.lezioni.complessita.esercizicasa;
+/*
+Scrivere un metodo ricorsivo che, dato un array di interi, restituisca il valore minimo (oppure il valore massimo)
+Scrivere un metodo ricorsivo che, dato un array di interi, restituisce la somma degli elementi (oppure altre operazioni)
+Scrivere un metodo ricorsivo che, data una matrice a di interi, restituisce la somma della sua diagonale principale
+Scrivere un metodo boolean che verifica se un intero è contenuto in un array
+Scrivere un programma che restituisca la somma corrispondente alla totalità di numeri dispari (oppure pari)  presenti all’interno di un array
+
+[BONUS] scrivere i rispettivi metodi iterativi
+*/
+
+public class EsCasa0906 {
+
+}
